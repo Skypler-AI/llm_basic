288fb863ee61de7b06b5243e2b35e2d992f77721
Skypler-AI <sriram@skypler.in>
2025-04-19 22:37:57 +0530

    Update transformers.py

--- transformers.py
+++ transformers.py
@@ -1,1 +1,28 @@
+# import 
 
+
+# input embeddings
+
+
+
+# positional encodings
+
+
+# layer norm
+
+
+# residual connection
+
+
+# multi head attention
+  # static method
+  # forward
+
+
+# encoder 
+
+
+# decoder
+
+
+# full transformers
